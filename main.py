dbad863a331e3ffeeefa4d5da3183ea7ab5eb088
Andrew <lungu.andrei.00@mail.ru>
2022-02-10 08:07:16 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+sir1 = "Acesta este un sir de caractere in Python"
+sir2 = "Salut, grupa IS31Z!"
+#print("sir1[0]: " ,sir1[0])
+'''print("sir2[4:9]: " ,sir2[3:9])
+
+print("Ultimul caracter ", sir2[-1])
+
+print('77' * 10)
+print(5 * 6)
+print(5.2 * 11)
+print(5.2 * 10)'''
+
+
